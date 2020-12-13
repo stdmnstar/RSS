73c79c9d173c150dbba693fcc5467d5126bf80c0
KirylZuyeu <kirylzuyeuimh@gmail.com>
2020-12-13 06:09:24 +0300

    feat: add core of app with state-managment-system

--- src/index.js
+++ src/index.js
@@ -1,8 +1,38 @@
 import './scss/index.scss';
 
-let button = document.querySelector('button');
+import store from './app/store/index.js'; 
 
-button.addEventListener('click', () => {
-  console.log('hello');
-})
+// Load up components
+import Count from './app/components/count.js';
+import List from './app/components/list.js';
+import Status from './app/components/status.js';
+
+// Load up some DOM elements
+const formElement = document.querySelector('.js-form');
+const inputElement = document.querySelector('#new-item-field');
+
+// Add a submit event listener to the form and prevent it from posting back
+formElement.addEventListener('submit', evt => {
+  evt.preventDefault();
+  
+  // Grab the text value of the textbox and trim any whitespace off it
+  let value = inputElement.value.trim();
+  
+  // If there's some content, trigger the action and clear the field, ready for the next item
+  if(value.length) {
+    store.dispatch('addItem', value);
+    inputElement.value = '';
+    inputElement.focus();
+  }
+});
+
+// Instantiate components
+const countInstance = new Count();
+const listInstance = new List();
+const statusInstance = new Status();
+
+// Initial renders
+countInstance.render();
+listInstance.render();
+statusInstance.render();
 
